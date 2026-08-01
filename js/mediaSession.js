7879ca0c492dafed02f8eec2d5f91ed84e37cc32
HauseMaster <113833707+HauseMasterZ@users.noreply.github.com>
2026-08-01 14:25:08 +0530

    UI: Strip obsolete Android revival hack and duplicate playbackState assignments from mediaSession.js handlers

--- js/mediaSession.js
+++ js/mediaSession.js
@@ -19,31 +19,9 @@
                 }
                 return;
             }
-            if (hasMediaSession) navigator.mediaSession.playbackState = 'playing';
-            audioPlayer.play().then(() => {
-                if (hasMediaSession) navigator.mediaSession.playbackState = 'playing';
-            }).catch(e => {
-                // Audio Element Revival: Android suspends the decoder during long pauses.
-                // Re-set src to force a reload, then seek back to the saved position.
-                const track = currentPlaylistData[playQueue[queueIndex]];
-                if (!track) {
-                    if (hasMediaSession) navigator.mediaSession.playbackState = 'paused';
-                    return;
-                }
-                const savedTime = audioPlayer.currentTime;
-                const onMeta = () => {
-                    audioPlayer.removeEventListener('loadedmetadata', onMeta);
-                    audioPlayer.currentTime = savedTime;
-                    audioPlayer.play().catch(() => {
-                        if (hasMediaSession) navigator.mediaSession.playbackState = 'paused';
-                    });
-                };
-                audioPlayer.addEventListener('loadedmetadata', onMeta);
-                audioPlayer.src = getAudioUrl(track);
-            });
+            audioPlayer.play().catch(e => console.warn("MediaSession play error:", e));
         });
         navigator.mediaSession.setActionHandler('pause', () => {
-            if (hasMediaSession) navigator.mediaSession.playbackState = 'paused';
             audioPlayer.pause();
         });
         navigator.mediaSession.setActionHandler('previoustrack', () => playPrev());
