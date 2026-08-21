a89d7f79f1994c852815730609b5f77127e794c7
HauseMaster <113833707+HauseMasterZ@users.noreply.github.com>
2026-08-21 11:26:43 +0530

    REVERT fix: playback pause 4

--- js/dom.js
+++ js/dom.js
@@ -25,37 +25,32 @@
                            'waiting', 'canplay', 'ended', 'durationchange'];
 
             this.forwardEvent = (e) => {
-                if (this.switching) {
-                    if (e.type === 'playing' || e.type === 'canplay') {
-                        this.switching = false;
-                        this.dispatchEvent(new Event(e.type));
+                if (!this.switching) {
+                    if (e.type === 'timeupdate') {
+                        const ct = this.active.currentTime;
+                        const dur = this.active.duration;
+                        if (dur > 0 && ct < dur - 1.0) {
+                            this._endedFired = false;
+                        }
+                        if (dur > 0 && this.lastKnownTime > 0 && !this.active.seeking) {
+                            if (!this._endedFired && ct >= dur - 0.25) {
+                                this._endedFired = true;
+                                this.dispatchEvent(new Event('ended'));
+                                return;
+                            }
+                        }
+                        this.lastKnownTime = ct;
                     }
-                    return;
-                }
-                if (e.type === 'timeupdate') {
-                    const ct = this.active.currentTime;
-                    const dur = this.active.duration;
-                    if (dur > 0 && ct < dur - 1.0) {
-                        this._endedFired = false;
-                    }
-                    if (dur > 0 && this.lastKnownTime > 0 && !this.active.seeking) {
-                        if (!this._endedFired && ct >= dur - 0.25) {
+                    if (e.type === 'ended') {
+                        if (!this._endedFired) {
                             this._endedFired = true;
                             this.dispatchEvent(new Event('ended'));
                             return;
                         }
-                    }
-                    this.lastKnownTime = ct;
-                }
-                if (e.type === 'ended') {
-                    if (!this._endedFired) {
-                        this._endedFired = true;
-                        this.dispatchEvent(new Event('ended'));
                         return;
                     }
-                    return;
+                    this.dispatchEvent(new Event(e.type));
                 }
-                this.dispatchEvent(new Event(e.type));
             };
             this.events.forEach(evt => {
                 this.active.addEventListener(evt, this.forwardEvent);
@@ -374,6 +369,13 @@
                     if (!preventAutoplay) {
                         this.active.play().catch(e => console.warn("MSE fast-start play error:", e));
                     }
+
+                    this.switching = false;
+                    this.dispatchEvent(new Event('loadedmetadata'));
+                    this.dispatchEvent(new Event('canplay'));
+                    this.dispatchEvent(new Event('play'));
+                    this.dispatchEvent(new Event('playing'));
+                    this.dispatchEvent(new Event('progress'));
 
                     // Single continuous background ingestion stream
                     (async () => {
