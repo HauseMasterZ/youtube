73a168c8907fa8e0b50a2b7b1c8904b63c3750bb
HauseMaster <113833707+HauseMasterZ@users.noreply.github.com>
2026-08-24 11:04:42 +0530

    fix: dominant color calc 5

--- js/state.js
+++ js/state.js
@@ -1,3 +1,8 @@
+    // Environment Feature Checks
+    const hasMediaSession = 'mediaSession' in navigator;
+    const hasTouch = 'ontouchstart' in window || (navigator.maxTouchPoints > 0);
+    const isMobileDevice = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
     const ALL_PLAYLISTS = ["Gym", "Driving", "Songs"];
     let allDatabases = {
         Gym: null,
