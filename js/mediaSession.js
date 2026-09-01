f8b6133b070a7fd0b7039985f964d7fef30217c2
HauseMaster <113833707+HauseMasterZ@users.noreply.github.com>
2026-09-01 18:26:04 +0530

    fix: audio focus mode 2 play 2

--- js/mediaSession.js
+++ js/mediaSession.js
@@ -436,14 +436,22 @@
             cancelAutoKillWatchdog();
             const playPromise = audioPlayer.play();
             if (playPromise && playPromise.then) {
-                playPromise.catch(e => {
+                playPromise.then(() => {
+                    stopLiveAudioAnchor();
+                    cancelAutoKillWatchdog();
+                }).catch(e => {
                     console.warn("MediaSession play error:", e);
+                    stopLiveAudioAnchor();
+                    cancelAutoKillWatchdog();
                     Promise.resolve().then(() => {
                         if (audioPlayer && (audioPlayer.paused || window.wasPausedByUser)) {
                             audioPlayer.play().catch(() => {});
                         }
                     });
                 });
+            } else {
+                stopLiveAudioAnchor();
+                cancelAutoKillWatchdog();
             }
         });
 
@@ -464,18 +472,24 @@
                         if (typeof updateTimeUI === 'function') updateTimeUI(0);
                         if (typeof lyricsActive !== 'undefined' && lyricsActive && typeof updateLyricsUI === 'function') updateLyricsUI(0);
                     }
-                    stopLiveAudioAnchor();
-                    cancelAutoKillWatchdog();
                     const playPromise = audioPlayer.play();
                     if (playPromise && playPromise.then) {
-                        playPromise.catch(e => {
+                        playPromise.then(() => {
+                            stopLiveAudioAnchor();
+                            cancelAutoKillWatchdog();
+                        }).catch(e => {
                             console.warn("MediaSession play error:", e);
+                            stopLiveAudioAnchor();
+                            cancelAutoKillWatchdog();
                             Promise.resolve().then(() => {
                                 if (audioPlayer && (audioPlayer.paused || window.wasPausedByUser)) {
                                     audioPlayer.play().catch(() => {});
                                 }
                             });
                         });
+                    } else {
+                        stopLiveAudioAnchor();
+                        cancelAutoKillWatchdog();
                     }
                 } else {
                     // User intentionally pausing in Mode 2
@@ -560,18 +574,24 @@
                             updateLyricsUI(0);
                         }
                     }
-                    stopLiveAudioAnchor();
-                    cancelAutoKillWatchdog();
                     const playPromise = audioPlayer.play();
                     if (playPromise && playPromise.then) {
-                        playPromise.catch(e => {
+                        playPromise.then(() => {
+                            stopLiveAudioAnchor();
+                            cancelAutoKillWatchdog();
+                        }).catch(e => {
                             console.warn("MediaSession playpause error:", e);
+                            stopLiveAudioAnchor();
+                            cancelAutoKillWatchdog();
                             Promise.resolve().then(() => {
                                 if (audioPlayer && (audioPlayer.paused || window.wasPausedByUser)) {
                                     audioPlayer.play().catch(() => {});
                                 }
                             });
                         });
+                    } else {
+                        stopLiveAudioAnchor();
+                        cancelAutoKillWatchdog();
                     }
                 } else {
                     window.wasPausedByUser = true;
